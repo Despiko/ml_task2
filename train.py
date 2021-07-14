bde42727f2ff25eb0608bdddeddfd05fc01cc7a0
despiko <artomonik@gmail.com>
2021-07-14 20:46:17 +0300

    setup dvc

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.metrics import f1_score, confusion_matrix, roc_auc_score
+from nltk.corpus import stopwords
+import json
 
 clean_data = pd.read_csv('cleaned.csv')
 
@@ -17,6 +19,10 @@ def lemmatize(text):
     return lemm_text
 
 corpus = list(clean_data['comment_text'].apply(lambda x: lemmatize(x)))
+
+nltk.download('stopwords')
+
+stopwords = set(stopwords.words('english'))
 
 count_tf_idf = TfidfVectorizer(stop_words = stopwords)
 tf_idf = count_tf_idf.fit_transform(corpus)
@@ -33,11 +39,15 @@ model_reg.fit(X_train, y_train)
 
 predict_linear = model_reg.predict(X_test)
 
-roc_auc_score(y_test, predict_linear)
+roc_auc = roc_auc_score(y_test, predict_linear)
 
 f1_lin = f1_score(y_test, predict_linear)
-print('F1 логистической регрессии: {:.4f}'.format(f1_lin))
-print()
-print('Матрица ошибок')
-print(confusion_matrix(y_test, predict_linear))
-print()
+with open("metrics.json", 'w') as outfile:
+    json.dump({ "F1": f1_lin, "roc_auc": roc_auc}, outfile)
+
+with open("report.txt", "w") as file:
+    print('F1 логистической регрессии: {:.4f}'.format(f1_lin), file=file)
+    print('',file=file)
+    print('Матрица ошибок', file=file)
+    print(confusion_matrix(y_test, predict_linear), file=file)
+    print('',file=file)
