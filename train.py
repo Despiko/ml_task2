11a376915ef913ec95f3bc1bdc59b6ad0265a405
Artem Nikulchev <51775214+Despiko@users.noreply.github.com>
2021-07-15 18:53:18 +0300

    Update train.py

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ target = clean_data['toxic'].values
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
 X_valid, X_test, y_valid, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
 
-model_reg = LogisticRegression(class_weight = 'balanced')
+model_reg = LogisticRegression(class_weight = 'balanced', C=0.5)
 
 model_reg.fit(X_train, y_train)
 
