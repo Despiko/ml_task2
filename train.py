ef5bca11cc6be67faa3f8f3f0232e7c396f0ad05
Artem Nikulchev <51775214+Despiko@users.noreply.github.com>
2021-07-15 17:18:10 +0300

    Update train.py

--- train.py
+++ train.py
@@ -35,10 +35,10 @@ tf_idf = count_tf_idf.fit_transform(corpus)
 features = tf_idf
 target = clean_data['toxic'].values
 
-X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.3, random_state=42)
 X_valid, X_test, y_valid, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
 
-model_reg = LogisticRegression(class_weight = 'balanced')
+model_reg = LogisticRegression(class_weight = 'balanced', C=0.5)
 
 model_reg.fit(X_train, y_train)
 
